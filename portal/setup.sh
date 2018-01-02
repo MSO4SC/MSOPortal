8e143a9cfcac29cd74674ea3d9e5dbd49ac2abd9
Javier Carnero <javier.carnero@atos.net>
2018-01-02 19:09:21 +0100

    Portal windows deployment docs

--- portal/setup.sh
+++ portal/setup.sh
@@ -14,5 +14,4 @@ sed -i 's/urlparse\./urllib.parse./g' $1/cloudify_rest_client/*.py
 sed -i 's/urllib\.quote/urllib.parse.quote/g' $1/cloudify_rest_client/*.py
 sed -i 's/urlsafe_b64encode(credentials)/urlsafe_b64encode(credentials.encode("utf-8"))/g' $1/cloudify_rest_client/client.py
 sed -i 's/+ encoded_credentials/+ str(encoded_credentials, "utf-8")/g' $1/cloudify_rest_client/client.py
-sed -i 's/+ encoded_credentials/+ str(encoded_credentials, "utf-8")/g' $1/cloudify_rest_client/client.py
 sed -i '/self.response = response/a\        self.message = message' $1/cloudify_rest_client/exceptions.py
