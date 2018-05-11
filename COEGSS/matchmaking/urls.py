b97319d00528ce2e48bd8d03150de90c05f77267
Javier Carnero <javier.carnero@atos.net>
2018-05-11 17:17:35 +0200

    Added template.env file to ckan #43
    - Minor change in docker compose
    - Old files from docker and COEGSS removed
    - READMEs updated

--- COEGSS/matchmaking/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-
-from . import views
-
-app_name = 'matchmaking'
-urlpatterns = [
-    # ex: /matchmaking/
-    url(r'^$', views.matchmaking, name='matchmaking'),
-]
