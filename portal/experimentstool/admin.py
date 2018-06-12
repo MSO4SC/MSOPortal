039de7cfd7cbbb25bf6fcd0aac7ce6c463cc2534
Javier Carnero <javier.carnero@atos.net>
2018-06-12 12:17:42 +0200

    Minor bug registering the models in admin

--- portal/experimentstool/admin.py
+++ portal/experimentstool/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from .models import Application, AppInstance, Execution, HPCInfrastructure
+from .models import Application, AppInstance, WorkflowExecution, HPCInfrastructure
 
 admin.site.register(Application)
 admin.site.register(AppInstance)
-admin.site.register(Execution)
+admin.site.register(WorkflowExecution)
 admin.site.register(HPCInfrastructure)
