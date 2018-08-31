40af0d92269c5bb61b8f2faaf6e626f1a0404c02
Javier Carnero <javier.carnero@atos.net>
2018-08-31 09:55:41 +0200

    [IDM] Improved renew_certs script

--- idm/renew_certs.sh
+++ idm/renew_certs.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-# Copyright 2018 MSO4SC - javier.carnero@atos.net
+########
+# Copyright (c) 2017-2018 MSO4SC - javier.carnero@atos.net
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#   http://www.apache.org/licenses/LICENSE-2.0
+#        http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -14,23 +15,13 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-if [ "$#" -lt 2 ]; then
-    echo "Usage: "$0" [email] [[host] ..]"
-    exit
-fi
+set -e
 
-apt-get update
-apt-get install -y software-properties-common
-add-apt-repository -y ppa:certbot/certbot
-apt-get update
-apt-get install -y certbot 
+echo "Warning: This command has to be executed with MSOPortal stopped"
+read -n 1 -s -r -p "Press any key to continue"
 
-COMMAND="certbot certonly --standalone --agree-tos -m $1"
-
-for host in "${@:2}"
-do
-    COMMAND="$COMMAND -d $host"
-done
-
-# generate the keys
-eval $COMMAND
+systemctl stop nginx
+docker-compose stop
+certbot renew
+docker-compose start
+systemctl start nginx
