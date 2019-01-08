229cc636337ee7e186ca883e4aac6d423e0d0b99
Javier Carnero <javier.carnero@atos.net>
2019-01-08 10:57:22 +0000

    Portal dockerized

---
+++ web/portal/setup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+
+if [ "$#" -ne 1 ]; then
+    echo "Usage: "$0" [python-packages-dir]"
+    echo "   "$0" /usr/local/lib/python3.5/dist-packages"
+    exit
+fi
+
+pip3 install -r requirements.txt
+
+# Hack to be python3 compatible
+PYPKG=$1
+sed -i 's/import urlparse/#import urlparse/g' $PYPKG/cloudify_rest_client/*.py
+sed -i 's/urlparse\./urllib.parse./g' $PYPKG/cloudify_rest_client/*.py
+sed -i 's/urllib\.quote/urllib.parse.quote/g' $PYPKG/cloudify_rest_client/*.py
+
+sed -i 's/import urlparse/#import urlparse/g' $PYPKG/cloudify_rest_client/aria/*.py
+sed -i 's/urlparse\./urllib.parse./g' $PYPKG/cloudify_rest_client/aria/*.py
+sed -i 's/urllib\.quote/urllib.parse.quote/g' $PYPKG/cloudify_rest_client/aria/*.py
+
+sed -i 's/urlsafe_b64encode(credentials)/urlsafe_b64encode(credentials.encode("utf-8"))/g' $PYPKG/cloudify_rest_client/client.py
+sed -i 's/+ encoded_credentials/+ str(encoded_credentials, "utf-8")/g' $PYPKG/cloudify_rest_client/client.py
+sed -i '/self.response = response/a\        self.message = message' $PYPKG/cloudify_rest_client/exceptions.py
